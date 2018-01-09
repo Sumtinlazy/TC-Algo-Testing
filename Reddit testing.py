95fa1bed280d905318b008e15ebb443976eeeabd
Thomas <TCernosia@github.com>
2018-01-09 22:41:36 +0800

    Broken Attempts

--- Reddit testing.py
+++ Reddit testing.py
@@ -1,15 +1,15 @@
 import praw
 
 
-reddit = praw.Reddit(client_id='5em6g0E9SMxX_A',
-                     client_secret='-jaB37bRUFI9ehOsgyXSwkifW5g',
+reddit = praw.Reddit(client_id='yXuNREmVQ7OB6A',
+                     client_secret='ToO_zFDTkkkW9vssW5WI2fwRPO4',
                      password='Thomas123',
                      user_agent='Amazing',
                      username='Murlaze')
 
-reddit = praw.Reddit(client_id='5em6g0E9SMxX_A',
-                     client_secret='-jaB37bRUFI9ehOsgyXSwkifW5g',
+reddit = praw.Reddit(client_id='yXuNREmVQ7OB6A',
+                     client_secret='ToO_zFDTkkkW9vssW5WI2fwRPO4',
                      redirect_uri='http://localhost:8080',
-                     user_agent='Wawaw')
-
-
+                     user_agent='testscript by /u/fakebot3')
+print(reddit.auth.url(['identity'], '...', 'permanent'))
+print(reddit.user.me())
--- algo.py
+++ algo.py
@@ -3,26 +3,56 @@ import requests
 from time import time
 import base64
 import json
+import ccxt
+from time import sleep
 
+#dc955e1e-12a6-4396-9c1e-bebb21c23b1a is secret
 
-def signature(): #dc955e1e-12a6-4396-9c1e-bebb21c23b1a is secret
-    host = 'https://api.kucoin.com'
-    endpoint = '/v1/KCS-BTC/order'
-    secret = 'dc955e1e-12a6-4396-9c1e-bebb21c23b1a'
-    string = endpoint+'/'+secret
-    string = base64.b64encode(string)
-    return string
-
-
-Authorize = {
-    "KC-API-KEY": "59c5ecfe18497f5394ded813",
-    "KC-API-NONCE": time(),
-    "KC-API-SIGNATURE": signature()
+'''
+average = []
+trend = []
+coins = 'ETH'
+price = requests.get('https://api.kucoin.com/v1/open/currencies', params = coins)
+data = urlopen(price).read()
+data = json.loads(data)
+USD = data['data']['rates']['BTC']['USD']
+print data
+parameters = {
+    type: BUY,
+    amount: 10,
+    price: 1.1
 }
-KEY = requests.get('https://api.kucoin.com/v1/api/list',params=Authorize)
+'''
 
 
-request = Request('https://api.kucoin.com/v1/open/currencies')
+def signature(order, amount, price):
+    type = order
+    
 
-response_body = urlopen(request).read()
-print response_body
+    r = Request('https://api.kucoin.com/')
+    r.headers = {
+        "KC-API-KEY": "5a30780191ed297ad231d9f8",
+        "KC-API-NONCE" : time(),
+        "KC-API-SIGNATURE" : signature(parameters)
+}
+
+
+'''
+while True:
+    if len(average) < 10 :
+        average.append(USD)
+    else:
+        del average[0]
+        average.append(USD)
+    data = urlopen(price).read()
+    data = json.loads(data)
+    USD = data['data']['rates']['BTC']['USD']
+    print(average)
+    if len(average) == 10:
+        maverage = (float(average[0]))/int(average[9])-1
+        print maverage, "% <-- growth in last 10 ticks"
+        trend.append(maverage)
+        print average[0] - average[9]
+    price = Request('https://api.kucoin.com/v1/open/currencies')
+    sleep(10)
+'''
