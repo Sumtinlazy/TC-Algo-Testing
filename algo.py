5fb32ad3976a4767940b3151e98708b1257f16a7
Thomas <TCernosia@github.com>
2018-01-17 17:50:24 +0800

    Authentication Framework
    New functions and stuff, framework for posting data

--- algo.py
+++ algo.py
@@ -23,21 +23,80 @@ def movingaverage(values, window):
     sma = np.convolve(values, weights, 'valid')
     return sma
 
+def authenticate(type, amount, price):
+
 
 dataRPX = list()
-trend = 0
+maverage = list()
+changeRPX = list()
+
 # this is the main loop
 while True:
     # add latest price to price list
     dataRPX.append(priceRPX())
-    print('current price:',priceRPX())
-    if len(dataRPX) >= 60:
+    print('current price:',priceRPX()),len(dataRPX)
+
+    if len(dataRPX) > 60:
         # find average price from last minute
         trend = movingaverage(dataRPX, 60)
-        print('trend:',trend[len(trend)-1])
+        print('trend last minute:',(dataRPX[len(dataRPX)]-dataRPX[len(dataRPX)-60])/dataRPX[len(dataRPX)-60]*100) + '%'
+
+
+
     sleep(1)
 
 # noinspection PyUnreachableCode
+
+class Client():
+    BASE_URL = 'api.kucoin.com'
+    def __init__(self, key = None, secret = None):
+        if key is None:
+            key = DEFAULT_KEY
+        #also for secret
+        self.key = key
+
+
+def params_to_str():
+    pass
+
+def _get(endpoint, **parmas):
+    prams_str = ''
+    url = BASE_URL + '?' + params_str
+    #send GET request to that url
+    date = parse('')
+    pass
+
+def parse(data):
+    pass
+
+def _sign(endpoint, nonce, str_params_for_sign):
+    #use SHA_256 to encrypto
+    str_to_sign = endpoint + '/' + nonce + '/' + str_params_for_sign
+    str_to_sign = base64.b64encode(str_to_sign.encode('utf-8'))
+    signed_str = hmac.new(secret.encode(), str_to_sign, hashlib.sha256).hexdigest()
+    return signed_str
+
+def _nonce():
+    return
+
+def _post(endpoint, payload):
+    #organize header
+    header = {}
+    payload = {}
+    url = BASE_URL + '/' + endpoint
+    signed_str = _sign(endpoint, nonce, str_params_for_sign)
+    #post your request to url
+    header['KC-API-SIGNATURE'] = signed_str
+    #add key and nonce
+
+    data = ''
+    #parse
+    return
+
+
+def create_limit_order(symbol, side, price, amount):
+    pass
+
 '''
 average = []
 trend = []
@@ -53,8 +112,6 @@ parameters = {
     price: 1.1
 }
 
-
-
 def signature(order, amount, price):
     type = order
 
@@ -65,7 +122,6 @@ def signature(order, amount, price):
         "KC-API-NONCE" : time(),
         "KC-API-SIGNATURE" : signature(parameters)
 }
-
 
 while True:
     if len(average) < 10 :
