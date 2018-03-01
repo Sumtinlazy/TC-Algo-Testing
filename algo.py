794ac66562f7432c08b2ef81e442344be2f985c8
Thomas <TCernosia@github.com>
2018-03-01 17:31:18 +0800

    March 1st
    Organized functions into the kucoin class. Also fixed the moving average function and started the info class.

--- algo.py
+++ algo.py
@@ -47,8 +47,10 @@ while True:
 # noinspection PyUnreachableCode
 
 
-class Client():
+class kucoin():
     BASE_URL = 'api.kucoin.com'
+    secret = 'dc955e1e-12a6-4396-9c1e-bebb21c23b1a'
+    key = '5a30780191ed297ad231d9f8'
 
     def __init__(self, key, secret):
         if key is None:
@@ -56,113 +58,127 @@ class Client():
         # also for secret
         self.key = key
 
+    def params_to_str(self, params):
+        sign = ''
+        keys = list()
+        for i in range(0, len(params)):
+            keys.append(list(params.keys())[i])
+            keys = sorted(keys)
+        for i in range(0, len(params)):
+            sign = sign + keys[i] + '=' + params[keys[i]] + '&'
+        sign = sign[:-1]
+        return sign
 
-# Alphabetizes dictionary and converts to string
-def params_to_str(params):
-    sign = ''
-    keys = list()
-    for i in range(0, len(params)):
-        keys.append(list(params.keys())[i])
-        keys = sorted(keys)
-    for i in range(0,len(params)):
-        sign = sign+keys[i]+'='+params[keys[i]]+'&'
-    sign = sign[:-1]
-    return sign
+    def _get(self, endpoint, payload):
+        header = {
+            'Accept-Language': 'en_US',
+            'Content-type': 'application/json'
+        }
+        url = 'http://api.kucoin.com' + endpoint
+        # send GET request to that url
+        header['KC-API-NONCE'] = str(int(time() * 1000))
+        header['KC-API-SIGNATURE'] = self._sign(self, endpoint, self.params_to_str(self, payload), header['KC-API-NONCE'])
+        header['KC-API-KEY'] = self.key
+        request = requests.get(url + "?" + self.params_to_str(payload), params=payload, headers=header)
+        response_body = request.json()
+        return response_body
 
+    def _sign(self, endpoint, str_params_for_sign, nonce):
+        # use SHA_256 to encrypto
+        str_to_sign = endpoint + '/' + nonce + '/' + str_params_for_sign
+        str_to_sign = base64.b64encode(str_to_sign.encode('utf-8'))
+        signed_str = hmac.new(self.secret.encode(), str_to_sign, hashlib.sha256).hexdigest()
+        return signed_str
 
-def _get(endpoint, payload):
-    header = {
-        'Accept-Language': 'en_US',
-        'Content-type': 'application/json'
-    }
-    url = 'http://api.kucoin.com' + endpoint
-    # send GET request to that url
-    header['KC-API-NONCE'] = str(int(time()*1000))
-    header['KC-API-SIGNATURE'] = _sign(endpoint, params_to_str(payload),header['KC-API-NONCE'])
-    header['KC-API-KEY'] = key
-    request = requests.get(url + "?" + params_to_str(payload), params=payload, headers=header)
-    response_body = request.json()
-    return response_body
+    def _post(self, endpoint, payload):
+        # organize header
+        header = {
+            'Accept-Language': 'en_US',
+            'Content-type': 'application/json'
+        }
+        url = 'https://api.kucoin.com' + endpoint
+        header['KC-API-NONCE'] = str(int(time() * 1000))
+        signed_str = self._sign(self, endpoint, self.params_to_str(self, payload), header['KC-API-NONCE'])
+        # post your request to url
+        header['KC-API-SIGNATURE'] = signed_str
+        # add key and nonce
+        header['KC-API-KEY'] = self.key
+        data = ''
+        print('arguments are', url + '?' + self.params_to_str(self, payload), header)
+        request = requests.post(url + '?' + self.params_to_str(self, payload), data=json.dumps({}), headers=header)
+        response_body = request.json()
+        print('Latency (ms):', int(header['KC-API-NONCE']) - int(time() * 1000))
+        print(payload)
+        return response_body
 
-
-def parse(data):
-    return ''
-    pass
-
-
-def _sign(endpoint, str_params_for_sign,nonce):
-    # use SHA_256 to encrypto
-    str_to_sign = endpoint + '/' + nonce + '/' + str_params_for_sign
-    str_to_sign = base64.b64encode(str_to_sign.encode('utf-8'))
-    signed_str = hmac.new(secret.encode(), str_to_sign, hashlib.sha256).hexdigest()
-    return signed_str
-
-
-def _post(endpoint, payload):
-    # organize header
-    header = {
-        'Accept-Language':'en_US',
-        'Content-type':'application/json'
-    }
-    url = 'https://api.kucoin.com' + endpoint
-    header['KC-API-NONCE'] = str(int(time()*1000))
-    signed_str = _sign(endpoint,params_to_str(payload), header['KC-API-NONCE'])
-    # post your request to url
-    header['KC-API-SIGNATURE'] = signed_str
-    # add key and nonce
-    header['KC-API-KEY'] = key
-    data = ''
-    print('arguments are', url+'?'+params_to_str(payload), header)
-    request = requests.post(url+'?'+params_to_str(payload),data=json.dumps({}),headers=header)
-    response_body = request.json()
-    print('Latency (ms):',int(header['KC-API-NONCE'])-int(time()*1000))
-    print(payload)
-    return response_body
-
-
-def create_limit_order(symbol, side, price, amount):
-    pass
+    # get kline data
+    def historic(self, symbol, first, last):
+        payload = {
+            'symbol': symbol,
+            'from': first,
+            'to': last
+        }
+        r = requests.get('https://api.kucoin.com/v1/open/chart/history', params=payload)
+        jdata = r.json()
+        return jdata
 
 
-requests.post('https://api.kucoin.com/v1/order',params={'amount': '10',
-    'price': '.00023',
-    'type': 'BUY'})
+class TestClient():
+    def __init__(self, exchange, currency, quote_currency):
+        #initialize the exchange client for kucoin
+        client = Client(None, None)
+        #initialize market
+        pass
+    def test_ticker(self):
+        #get ticker
+        ticker = self.client.get_ticker()
+        assert ticker is not None
+        assert isinstance(ticker['last'], [float, int])
+        assert 'bid' in ticker.keys()
+        #verify ticker
 
-# historic data
+        pass
 
+    def test_order(self):
+        #get active orders
+        active_orders = client.get_active_orders(self.market)
+        for order_id, value in active_orders.items():
+            #cancel it
+            pass
+            #check the order info
+            order_info = self.client.get_order_info(order_id, self.market, 'buy')
+            #check the balance
+            #re-create the order at the same price with same volume
 
-def historic(symbol,first, last):
-    payload = {
-        'symbol': symbol,
-        'from': first,
-        'to': last
-    }
-    request = requests.get('https://api.kucoin.com/v1/open/chart/history', params=payload)
-    jdata = request.json()
-    return jdata
+    def test_rate_limit(self):
+        try_times = 0
+        while(try_times <= 500):
+            #get balacne'
+            try_times += 1
 
+        #test public endpoints
 
-data = historic('RPX-ETH', 1250000000,int(time()))
+    def run(self):
+        pass
 
-df = pd.DataFrame({
-    'open':data['c'],
-    'tstamp':data['t'],
-    'volume':data['v'],
-    'high':data['h'],
-    'close':data['o'],
-    'low':data['l']
-})
+'''
+print(kucoin._post(kucoin, '/v1/order', {
+    'symbol':'RPX-ETH',
+    'type':'BUY',
+    'amount': '10',
+    'price': '0.000001'
+}))
+print(kucoin._get(kucoin, '/v1/order/active', {
+    'symbol':'RPX-ETH',
+    'type':'BUY'
+}))
+'''
 
+class info():
+    def movingaverage(values,window):
+        sum = 0
+        for i in range(0,window):
+            sum += values[len(values)-window+i]
+        movingaverage = sum/window
+        return movingaverage
 
-def movingaverage(values,window):
-    maverage = []
-    for i in range(0,len(values)):
-        maverage.append(sum(values[i:window+i])/window)
-
-    return maverage
-
-# bollinger bands
-
-test = range(0,200)
-print(movingaverage(test,10))
-print(sum(test[190:200]))
